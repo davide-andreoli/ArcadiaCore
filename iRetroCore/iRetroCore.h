2b4aa943041cf71c8d4d706d66242199b17f8d7f
Davide Andreoli <davide.andreoli@icloud.com>
2024-05-01 18:57:47 +0200

    Initial Commit

---
+++ iRetroCore/iRetroCore.h
@@ -0,0 +1,18 @@
+//
+//  iRetroCore.h
+//  iRetroCore
+//
+//  Created by Davide Andreoli on 01/05/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iRetroCore.
+FOUNDATION_EXPORT double iRetroCoreVersionNumber;
+
+//! Project version string for iRetroCore.
+FOUNDATION_EXPORT const unsigned char iRetroCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iRetroCore/PublicHeader.h>
+
+
